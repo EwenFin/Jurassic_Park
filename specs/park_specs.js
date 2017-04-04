1d9d9248d614b95f72929235a476b8cf24f90d57
Ewen Carr <ewencarr@hotmail.com>
2017-04-04 16:40:11 +0100

    park can add dinosaurs

--- specs/park_specs.js
+++ specs/park_specs.js
@@ -1,15 +1,33 @@
 var assert = require("assert");
 var Park = require("../park.js");
+var Dinosaur = require("../dinosaur.js")
 
 describe("Jurassic Park", function(){
   var park;
+
+  var dinosaur1;
+  var dinosaur2;
+  var dinosaur3;
+
   beforeEach(function(){
     park = new Park();
+
+    dinosaur1 = new Dinosaur("T-Rex", 1);
+    dinosaur2 = new Dinosaur("Velociraptor", 4);
+    dinosaur3 = new Dinosaur("Triceraptops", 2);
 
   });
 
   it("Park starts empty", function(){
-    assert.strictEqual(0, park.enclosure.length)
-  })
+    assert.strictEqual(0, park.enclosure.length);
+  });
+
+  it("Park can add dinosaurs", function(){
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    assert.strictEqual(2, park.enclosure.length);
+  });
+
+
 
 });
